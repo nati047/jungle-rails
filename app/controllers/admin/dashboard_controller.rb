6c0461cebf7c9686c31a8ea03cfad44f3e59eb44
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-01-07 08:24:14 +0000

    add about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  # http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    @products = Product.count
+    @categories = Category.count  # query not working
   end
 end
