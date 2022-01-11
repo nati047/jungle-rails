74f1aae49d813a1f5672979084ccef0d40732ede
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-01-11 00:20:36 +0000

    implimet user auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20220110222608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,21 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "collaborators", force: :cascade do |t|
+    t.integer "collaborator_id"
+    t.integer "map_id"
+  end
+
+  create_table "favorite_maps", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "map_id"
+  end
+
+  create_table "favourite_maps", force: :cascade do |t|
+    t.integer "saved_from_map_id"
+    t.integer "saved_to_user_id"
   end
 
   create_table "line_items", force: :cascade do |t|
@@ -35,12 +50,37 @@ ActiveRecord::Schema.define(version: 20160625062916) do
   add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
+  create_table "maps", force: :cascade do |t|
+    t.integer "creator_id"
+    t.string  "name",        limit: 255,                                 null: false
+    t.text    "description",             default: "Enter a description"
+    t.string  "image_url",   limit: 255, default: "Enter a url"
+    t.string  "latitude",    limit: 255
+    t.string  "longitude",   limit: 255
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer  "total_cents"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "stripe_charge_id"
-    t.string   "email"
+  end
+
+  create_table "points", force: :cascade do |t|
+    t.string  "name",        limit: 255
+    t.integer "map_id"
+    t.text    "description"
+    t.string  "title",       limit: 255
+    t.string  "picture_url", limit: 255
+  end
+
+  create_table "points_of_interest", force: :cascade do |t|
+    t.integer "map_id"
+    t.text    "description",             default: "Enter a description"
+    t.string  "title",       limit: 255,                                 null: false
+    t.string  "picture_url", limit: 255, default: "Enter a url"
+    t.integer "latitude",                                                null: false
+    t.integer "longitude",                                               null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -56,7 +96,17 @@ ActiveRecord::Schema.define(version: 20160625062916) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "favourite_maps", "maps", column: "saved_from_map_id", name: "favourite_maps_saved_from_map_id_fkey", on_delete: :cascade
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "points_of_interest", "maps", name: "points_of_interest_map_id_fkey", on_delete: :cascade
   add_foreign_key "products", "categories"
 end
